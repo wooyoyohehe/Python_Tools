e05ad17f54f0adf85e1617c1517c7483921d1bf4
boomclap <852969799@qq.com>
2020-10-21 11:48:18 +0800

    Create dict_usage.py

---
+++ dict_usage.py
@@ -0,0 +1,25 @@
+# initialize a dict
+
+dict = {'a': 1, 'b': 2, 'b': '3'}
+# if there two valuse for the same key, choose the value of the last key
+
+# USE THE VALUE
+x = 'a'
+print type(x)
+print dict[x]+1
+
+
+dict1 = {'Name': 'Shuang', 'Age': 25, 'Class': 'First'}
+# update the exsisting key value
+dict1['Age'] = 18
+# add a new key value
+dict1['School'] = "Emory" 
+
+# return all the keys in the dict
+dict.keys
+
+# if the key is in the dict, return true
+dict.has_key()
+
+
+
